d308eb1a103a8da6fb1a11b3ccec2d65f69a5aa5
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2023-02-21 22:11:04 +0700

    api addressing

--- routes/api.php
+++ routes/api.php
@@ -22,11 +22,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+//katergori barang
 Route::get('barang/getAllKB/', [kategoriBarangController::class, 'getAllData']);
-Route::post('barang/addKategoriBarang/', [kategoriBarangController::class, 'insertKategoriBarang']);
+Route::post('barang/addKB/', [kategoriBarangController::class, 'insertKategoriBarang']);
 Route::post('barang/updateKB/{id}', [kategoriBarangController::class, 'updateKategoriBarang']);
 Route::post('barang/deleteKB/{id}', [kategoriBarangController::class, 'deleteKategoriBarang']);
+//barang
 Route::Get('barang/getAllBarang/', [barangController::class, 'getAllData']);
 Route::post('barang/addBarang/', [barangController::class, 'insertBarang']);
+Route::post('barang/updateBarang/{id}', [barangController::class, 'updateBarang']);
+Route::post('barang/deleteBarang/{id}', [barangController::class, 'deleteBarang']);
+//users barang
+Route::get('barang/getAllUsersBarang/', [users_barangController::class, 'getAllData']);
+Route::post('barang/addUsersBarang/', [users_barangController::class, 'insertusers_barang']);
+Route::post('barang/updateUsersBarang/{id}', [users_barangController::class, 'updateusers_barang']);
+Route::post('barang/deleteUsersBarang/{id}', [users_barangController::class, 'deleteusers_barang']);
+//users
+Route::get('barang/getAllUsers/', [usersController::class, 'getAllData']);
+Route::post('barang/addUsers/', [usersController::class, 'insertUsers']);
+Route::post('barang/updateUsers/{id}', [usersController::class, 'updateUsers']);
+Route::post('barang/deleteUsers/{id}', [usersController::class, 'deleteUsers']);
+
 
